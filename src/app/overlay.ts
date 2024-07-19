a7ad28fae966c11cdebdaddfc7dd5204bf9bcdce
nartc <nartc7789@gmail.com>
2024-07-18 23:00:19 -0500

    fix opacity

--- src/app/overlay.ts
+++ src/app/overlay.ts
@@ -11,8 +11,8 @@ import { scenes, slide } from "./state";
 	standalone: true,
 	template: `
 		<div
-			class="fixed z-10 top-0 left-0 bottom-0 right-0 flex flex-col justify-between pointer-events-none text-black transition-opacity duration-1000"
-			[class.opacity-0]="!visible()"
+			class="fixed z-10 top-0 left-0 bottom-0 right-0 flex flex-col justify-between pointer-events-none text-black transition-opacity duration-1000 "
+			[class]="!visible() ? ['opacity-0'] : []"
 		>
 			<svg
 				class="w-40 mx-auto mt-8"
@@ -64,7 +64,7 @@ import { scenes, slide } from "./state";
 				class="bg-gradient-to-t from-white/90 pt-20 pb-10 p-4 flex items-center flex-col text-center"
 			>
 				@let slide = scenes[displaySlide()];
-				
+
 				<h1 class="text-5xl font-extrabold">
 					{{ slide.name }}
 				</h1>
@@ -110,9 +110,7 @@ import { scenes, slide } from "./state";
 									d="M21 10.5h.375c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125H21M4.5 10.5H18V15H4.5v-4.5zM3.75 18h15A2.25 2.25 0 0021 15.75v-6a2.25 2.25 0 00-2.25-2.25h-15A2.25 2.25 0 001.5 9.75v6A2.25 2.25 0 003.75 18z"
 								/>
 							</svg>
-							<p class="font-semibold text-3xl">
-								{{ slide.range }}km
-							</p>
+							<p class="font-semibold text-3xl">{{ slide.range }}km</p>
 						</div>
 						<p class="text-sm opacity-80">With one single charge</p>
 					</div>
